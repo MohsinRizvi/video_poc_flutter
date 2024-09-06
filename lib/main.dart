0faa502d2dd4802635d19b31ab2d191e7024c961
syedmohsinhussainrizvi03 <141136632+syedmohsinhussainrizvi03@users.noreply.github.com>
2024-09-06 18:29:18 +0500

    better play

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,78 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const ReelsScreen(),
+        home:  HLSVideoPlayer(videoUrl: 'https://flipfit-cdn.akamaized.net/flip_hls/661f570aab9d840019942b80-473e0b/video_h1.m3u8',),
       ),
     );
   }
 }
 
 
+
+
+class HLSVideoPlayer extends StatefulWidget {
+  final String videoUrl;
+
+  HLSVideoPlayer({required this.videoUrl});
+
+  @override
+  _HLSVideoPlayerState createState() => _HLSVideoPlayerState();
+}
+
+class _HLSVideoPlayerState extends State<HLSVideoPlayer> {
+  late BetterPlayerController _betterPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    BetterPlayerDataSource dataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      widget.videoUrl,
+      liveStream: false, // Set to true if it's a live stream
+    );
+
+    _betterPlayerController = BetterPlayerController(
+      const BetterPlayerConfiguration(
+        autoPlay: true,
+        looping: false,
+        aspectRatio: 16 / 9,
+        fit: BoxFit.contain,
+        controlsConfiguration: BetterPlayerControlsConfiguration(
+          enableSkips: false,
+          enablePlayPause: true,
+          enableFullscreen: true,
+        ),
+      ),
+      betterPlayerDataSource: dataSource,
+    );
+  }
+
+  @override
+  void dispose() {
+    _betterPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'HLS Video Player',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: 9 / 9,
+          child: BetterPlayer(
+            controller: _betterPlayerController,
+          ),
+        ),
+      ),
+    );
+  }
+}
